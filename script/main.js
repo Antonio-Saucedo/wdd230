e70ca5657cbd8155484fd1191439b4ab0693af25
Antonio-Saucedo <antoniojesus@byui.edu>
2022-04-30 20:39:21 -0600

    Changes to main page commit

--- script/main.js
+++
@@ -1,5 +0,0 @@
-const d = new Date();
-const author = document.getElementById("author").textContent;
-
-document.getElementById("footer").textContent = `© ${d.getFullYear()} | ${author} | Idaho`;
-document.getElementById("date").textContent = `Last Updated: ${document.lastModified}`;
---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+const d = new Date();
+
+document.getElementById("footer").textContent = `© ${d.getFullYear()} | Antonio J. Saucedo | Idaho`;
+document.getElementById("date").textContent = `Last Updated: ${document.lastModified}`;
