d89104cf19c203b074d1c8097df0cb7868a3d90c
Antonio-Saucedo <antoniojesus@byui.edu>
2022-05-29 16:32:24 -0600

    Image optimization commit

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -8,10 +8,11 @@ const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
 
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
 const d = new Date();
 
-document.getElementById("today").textContent = `${weekday[d.getDay()]}, ${d.getDate()} ${d.getFullYear()}`;
+document.getElementById("today").textContent = `${weekday[d.getDay()]}, ${d.getDate()} ${month[d.getMonth()]} ${d.getFullYear()}`;
 document.getElementById("modified").textContent = `Last Updated: ${document.lastModified}`;
 
 if (d.getDay() == 1 || d.getDay() == 2) {
