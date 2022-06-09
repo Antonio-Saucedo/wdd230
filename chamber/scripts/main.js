c2edbd960ce96c24739d5640d34d3f5e038b20bd
Antonio-Saucedo <antoniojesus@byui.edu>
2022-06-09 01:52:23 -0600

    lazyload all commit

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -18,3 +18,38 @@ document.getElementById("modified").textContent = `Last Updated: ${document.last
 if (d.getDay() == 1 || d.getDay() == 2) {
   document.getElementById("joinUs").style.display = "block";
 }
+
+const images = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 50px 0px",
+};
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+if ("IntersectionObserver" in window) {
+  const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        loadImages(entry.target);
+        imgObserver.unobserve(entry.target);
+      }
+    });
+  }, imgOptions);
+
+  images.forEach((img) => {
+    imgObserver.observe(img);
+  });
+} else {
+  images.forEach((img) => {
+    loadImages(img);
+  });
+}
