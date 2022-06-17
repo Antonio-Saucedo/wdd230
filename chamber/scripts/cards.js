96173b585f90cbb53c35cf8976c06b51f365af73
Antonio-Saucedo <antoniojesus@byui.edu>
2022-06-17 00:43:38 -0600

    fixes commit

--- chamber/scripts/cards.js
+++ chamber/scripts/cards.js
@@ -4,72 +4,70 @@ const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
 const display = document.getElementById("cards");
 
-function buildBusinessCards(info) {
+function buildBusinessCards(info, type) {
   let data = info.businesses;
   data.forEach((business) => {
     let card = document.createElement("section");
-    let img = document.createElement("img");
     let p = document.createElement("p");
     let p2 = document.createElement("p");
     let p3 = document.createElement("p");
 
-    img.setAttribute("src", `${business.imageurl}`);
+    card.setAttribute("id", "section");
     p.innerHTML = `${business.address}`;
     p2.innerHTML = `${business.phone}`;
     p3.innerHTML = `${business.website}`;
-    img.setAttribute("loading", "lazy");
 
-    card.append(img);
+    if (type == "grid") {
+      let img = document.createElement("img");
+      img.setAttribute("src", `${business.imageurl}`);
+      img.setAttribute("alt", `${business.name}`);
+      img.setAttribute("loading", "lazy");
+      card.append(img);
+    } else {
+      let h2 = document.createElement("h2");
+      h2.innerHTML = `${business.name}`;
+      card.append(h2);
+    }
+
     card.appendChild(p);
     card.appendChild(p2);
     card.appendChild(p3);
 
-    display.classList.add("grid");
+    display.classList.add(type);
     display.append(card);
   });
 }
 
-async function getBusinesses() {
+async function getBusinesses(type) {
   let response = await fetch(URL);
   if (response.ok) {
     let data = await response.json();
-    buildBusinessCards(data);
+    buildBusinessCards(data, type);
   } else {
     throw Error(response.statusText);
   }
 }
 
-getBusinesses();
+function deleteItems() {
+  for (let i = 0; i < 9; i++) {
+    document.getElementById("section").remove();
+  }
+}
+
+getBusinesses("grid");
 
 gridbutton.addEventListener("click", () => {
   if (display.classList.value == "cards list") {
-    display.classList.add("grid");
+    deleteItems();
     display.classList.remove("list");
-    data.forEach((business) => {
-      let img = document.createElement("img");
-
-      img.setAttribute("src", `${business.imageurl}`);
-      img.setAttribute("loading", "lazy");
-
-      card.append(img);
-
-      display.append(card);
-    })
-  };
+    getBusinesses("grid");
+  }
 });
 
 listbutton.addEventListener("click", () => {
   if (display.classList.value == "cards grid") {
-    display.classList.add("list");
+    deleteItems();
     display.classList.remove("grid");
-    data.forEach((business) => {
-      let h2 = document.createElement("h2");
-
-      h2.innerHTML = `${business.name}`;
-
-      card.append(h2);
-
-      display.append(card);
-    });
-  };
+    getBusinesses("list");
+  }
 });
