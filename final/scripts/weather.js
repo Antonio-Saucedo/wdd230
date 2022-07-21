fa1fe80faff70327f0ddb8856a6f8d32e0fb5a21
Antonio-Saucedo <antoniojesus@byui.edu>
2022-07-21 00:38:22 -0600

    call to action button fix

--- final/scripts/weather.js
+++ final/scripts/weather.js
@@ -6,27 +6,26 @@ const weatherIcon3Day = document.querySelector("#weather-three-today");
 const weatherDesc = document.querySelector("#condition");
 const humidity = document.querySelector("#humidity");
 
-const url = `https://api.openweathermap.org/data/2.5/onecall?lat=40.55&lon=-91.3849&exclude=minutely,hourly&appid=fc6f9211ad045deb2b01c57bb94315bc`;
+const url = `https://api.openweathermap.org/data/2.5/onecall?lat=40.0003&lon=-89.2504&exclude=minutely,hourly&appid=fc6f9211ad045deb2b01c57bb94315bc`;
 
 apiFetch(url);
 
 function displayResults (weatherData) {
     currentTemp.innerHTML = Math.round((weatherData.current.temp.toFixed(0) - 32) * (5/9));
     const weatherimagesrc = `https://antonio-saucedo.github.io/wdd230/final/images/weatherIcons/${weatherData.current.weather[0].icon}.png`;
+    const weatherimagesrc2 = `https://antonio-saucedo.github.io/wdd230/final/images/weatherIcons/${weatherData.daily[1].weather[0].icon}.png`;
 
     // Capitalize first letter of each word.
-    const descweather = weatherData.weather[0].description;
+    const descweather = weatherData.current.weather[0].description;
     const descweatherdisplay = descweather.split(" ");
     for (var i = 0; i < descweatherdisplay.length; i++) {
         descweatherdisplay[i] = descweatherdisplay[i].charAt(0).toUpperCase() + descweatherdisplay[i].slice(1);
     }
-    const descweather2 = descweatherdisplay.join(" ");
 
-    const descwind = Math.round(weatherData.wind.speed.toFixed(0) * 1.609);
+    humidity.innerHTML = `${weatherData.current.humidity}%`;
     weatherIconToday.setAttribute("src", weatherimagesrc);
     weatherIconToday.setAttribute("alt", descweather);
-    weatherDesc.innerHTML = descweather2;
-    humidity.innerHTML = descwind;
+    weatherDesc.innerHTML = descweatherdisplay.join(" ");
 }
 
 async function apiFetch(apiURL) {
