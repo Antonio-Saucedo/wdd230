e121d32c5545ad3486a3fbb84ca2cff66b32b181
Antonio-Saucedo <antoniojesus@byui.edu>
2026-05-27 19:11:36 -0600

    lesson5 | Updated CSS to be block focused. Implemented dynamic sizing. Updated JavaScript functionality to save list items into local storage. Implemented prettier styling.

--- lesson5/scripts/main.js
+++ lesson5/scripts/main.js
@@ -1,33 +1,81 @@
-const list = document.querySelector("ul");
-const input = document.querySelector("input");
-const button = document.querySelector("button");
+const listContainer = document.querySelector("ul.list");
+const bookInput = document.getElementById("book-chapter-input");
+const addButton = document.getElementById("add-button");
+const headerLabel = document.querySelector("h1");
+let savedItems = JSON.parse(localStorage.getItem("listItems")) ?? [];
 
-// Use Enter key to submit new list items.
-input.addEventListener("keyup", function (event) {
-  if (event.key == "Enter") {
+// Initialize page elements
+savedItems.forEach(({ value, index }) => createListItemElement(value, index));
+toggleHeaderIcons();
+
+// Event listeners
+bookInput.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
     event.preventDefault();
-    button.click();
+    addListItem();
   }
 });
 
-button.addEventListener("click", () => {
-  const item = input.value;
-  input.value = "";
-  // Check if input value is blank with blank space check.
-  if (/^\s*$/.test(item) != "") {
-  } else {
-    const listItem = document.createElement("li");
-    const listBtn = document.createElement("button");
+addButton.addEventListener("click", addListItem);
 
-    listItem.textContent = item;
-    listItem.appendChild(listBtn);
-    listBtn.textContent = "X";
-    list.appendChild(listItem);
+// Functions
+function addListItem() {
+  const bookChapter = bookInput.value;
+  if (!bookChapter) return;
 
-    listBtn.addEventListener("click", () => {
-      list.removeChild(listItem);
-    });
+  bookInput.value = "";
 
-    input.focus();
-  }
-});
+  // Check if input value is empty with blank space check.
+  const newIndex =
+    savedItems.length === 0
+      ? 0
+      : Number(savedItems[savedItems.length - 1].index) + 1;
+
+  createListItemElement(bookChapter, newIndex);
+  savedItems.push({ index: newIndex, value: bookChapter });
+  saveItems();
+
+  bookInput.focus();
+  toggleHeaderIcons();
+}
+
+function createListItemElement(newItem, index) {
+  const listItem = document.createElement("li");
+  const listButton = document.createElement("button");
+  const listButtonImage = document.createElement("img");
+
+  // Add image to button element
+  listButtonImage.src = "./images/x.png";
+  listButtonImage.alt = "Delete Book Icon";
+  listButton.appendChild(listButtonImage);
+
+  // Add text and button to list element
+  listItem.textContent = newItem;
+  listItem.dataset.id = index;
+  listItem.appendChild(listButton);
+
+  // Add row to list container element
+  listContainer.appendChild(listItem);
+
+  listButton.addEventListener("click", () => {
+    savedItems = savedItems.filter((item) => item.index !== index);
+    saveItems();
+    listItem.remove();
+    toggleHeaderIcons();
+  });
+}
+
+function saveItems() {
+  localStorage.setItem("listItems", JSON.stringify(savedItems));
+}
+
+// Add book open/close animation
+function toggleHeaderIcons() {
+  const hasItems = listContainer.querySelector("li") !== null;
+  headerLabel.querySelector(".opened-book").style.display = hasItems
+    ? "none"
+    : "inline-block";
+  headerLabel.querySelector(".closed-book").style.display = hasItems
+    ? "inline-block"
+    : "none";
+}
