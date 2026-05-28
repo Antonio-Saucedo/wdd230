e7c07647ab2d2bf639692c39024ac489a4550154
Antonio-Saucedo <antoniojesus@byui.edu>
2026-05-28 11:21:35 -0600

    lesson7 | Added footer information. Fixed HTML bugs and added missing regions. Updated JavaScript code to be more concise, clear, and optimized. Implemented prettier styling.

--- lesson7/scripts/main.js
+++ lesson7/scripts/main.js
@@ -1,33 +1,28 @@
+const d = new Date();
+
+document.getElementById("footer").textContent =
+  `© ${d.getFullYear()} | Antonio J. Saucedo | Idaho`;
+
 const images = document.querySelectorAll("img[data-src]");
 
-const imgOptions = {
-  threshold: 1,
-  rootMargin: "0px 0px 50px 0px",
-};
-
-const loadImages = (image) => {
-  image.setAttribute("src", image.getAttribute("data-src"));
-  image.onload = () => {image.removeAttribute("data-src");
-  };
+const loadImage = (image) => {
+  image.src = image.dataset.src;
+  image.onload = () => image.removeAttribute("data-src");
 };
 
 if ("IntersectionObserver" in window) {
-  const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-    entries.forEach((entry) => {
-      if (!entry.isIntersecting) {
-        return;
-      } else {
-        loadImages(entry.target);
-        imgObserver.unobserve(entry.target);
-      }
-    });
-  }, imgOptions);
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) return;
+        loadImage(entry.target);
+        observer.unobserve(entry.target);
+      });
+    },
+    { threshold: 1, rootMargin: "0px 0px 50px 0px" },
+  );
 
-  images.forEach((img) => {
-    imgObserver.observe(img);
-  });
+  images.forEach((img) => observer.observe(img));
 } else {
-  images.forEach((img) => {
-    loadImages(img);
-  });
+  images.forEach(loadImage);
 }
