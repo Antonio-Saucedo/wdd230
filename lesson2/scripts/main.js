5a4fa777dfbf3c66ce35cbfb35869ca6ffb65e43
Antonio-Saucedo <antoniojesus@byui.edu>
2026-05-27 23:40:01 -0600

    Scripts | Moved script references to head

--- lesson2/scripts/main.js
+++ lesson2/scripts/main.js
@@ -1,3 +1,4 @@
 const d = new Date();
 
-document.getElementById("footer").textContent = `© ${d.getFullYear()} | Antonio J. Saucedo | Last Updated: ${document.lastModified}`;
+document.getElementById("footer").textContent =
+  `© ${d.getFullYear()} | Antonio J. Saucedo | Last Updated: ${document.lastModified}`;
