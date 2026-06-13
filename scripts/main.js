b0729b3c1e086722916ecef8bafa02cc93d8bb1a
Antonio-Saucedo <antoniojesus@byui.edu>
2026-06-12 20:35:14 -0600

    Pages | Fixed minor HTML and styling bugs. Added missing default fallback on font styles. Implemented prettier styling.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 const d = new Date();
 
-document.getElementById("footer").textContent = `© ${d.getFullYear()} | Antonio J. Saucedo | Idaho`;
-document.getElementById("date").textContent = `Last Updated: ${document.lastModified}`;
+document.getElementById("footer").textContent =
+  `© ${d.getFullYear()} | Antonio J. Saucedo | Idaho`;
+document.getElementById("date").textContent =
+  `Last Updated: ${document.lastModified}`;
